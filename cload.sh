4da0e8e9b84e5a3961a68ea5ae7cd2cb4dd3b319
Korneliusz Osmenda <korneliuszo@gmail.com>
2021-05-31 17:11:31 +0200

    Better soundcard settings

--- cload.sh
+++ cload.sh
@@ -4,9 +4,9 @@ AUDIOCARD=1
 AUDIODEVICE=0
 RMT_TTY="/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0"
 
-amixer -c $AUDIOCARD sset "Mic" 0% off
+amixer -c $AUDIOCARD sset "Mic" capture 100%
 amixer -c $AUDIOCARD sset "Speaker" 100%
-amixer -c $AUDIOCARD sset "Auto Gain Control" on
+amixer -c $AUDIOCARD sset "Auto Gain Control" off
 
 stty -F $RMT_TTY raw
 dd if=$RMT_TTY of=/dev/null bs=1 count=1
